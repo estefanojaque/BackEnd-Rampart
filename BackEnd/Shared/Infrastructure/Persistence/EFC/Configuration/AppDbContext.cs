013717e6fd7210effe8f6923a69bada62511be8a
sebastianrealcalderon <u20221d964@upc.edu.pe>
2024-11-04 17:04:07 -0500

    feat: updated AppDbContext.cs

--- BackEnd/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ BackEnd/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using catch_up_platform_firtness.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
+using BackEnd.Posts;
+using BackEnd.Posts.Domain.Model.Aggregates;
 
 namespace BackEnd.Shared.Infrastructure.Persistence.EFC.Configuration;
 
@@ -15,7 +17,14 @@ public class AppDbContext (DbContextOptions options) : DbContext(options)
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        
         builder.UseSnakeCaseNamingConvention();
+        
+        builder.Entity<Post>().ToTable("post");
+        builder.Entity<Post>().HasKey(up => up.id);
+        builder.Entity<Post>().Property(up => up.dishId).IsRequired().HasColumnName("dishId");
+        builder.Entity<Post>().Property(up => up.publishDate).IsRequired().HasColumnName("publishDate");
+        builder.Entity<Post>().Property(up => up.stock).IsRequired().HasColumnName("stock");
     }
+    
+    public DbSet<Post> Posts { get; set; }
 }
