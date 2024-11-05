b374e751cc5c28a0cffc83e0c8bfcb4471baf2f3
Estefano Oscar Jaque Peña <u202225466@upc.edu.pe>
2024-11-04 20:35:59 -0500

    Update Program.cs

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -86,10 +86,14 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "RideFind API V1");
+        c.RoutePrefix = string.Empty; // Esto hace que Swagger esté disponible en la raíz
+    });
 }
 
 app.UseHttpsRedirection();
